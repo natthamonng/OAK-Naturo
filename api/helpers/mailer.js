cf09d07356bc3ee20632e3ecab9311497338e388
Natthamon Kharoubi <natthamon.ng@gmail.com>
2020-04-08 00:54:28 +0200

    create helper password reset email using Nodemailer

---
+++ api/helpers/mailer.js
@@ -0,0 +1,37 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+class Mailer {
+
+    transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: `${process.env.EMAIL_ADDRESS}`,
+            pass: `${process.env.EMAIL_PASSWORD}`,
+        },
+    });
+
+    sendEmail = (user, subject, body) => {
+        return new Promise((resolve, reject) => {
+            const mailOptions = {
+                from: 'oakproject2020@gmail.com',
+                to: `${user.email}`,
+                subject: subject,
+                text: body
+            };
+            const self = this;
+            self.transporter.sendMail(mailOptions, (err, response) => {
+                if (err) {
+                    reject(err);
+
+                } else {
+                    resolve();
+                }
+            });
+        })
+    }
+}
+
+module.exports = Mailer;
+
+
