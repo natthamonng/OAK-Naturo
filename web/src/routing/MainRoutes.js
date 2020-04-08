32dfd8c3f50158d3b5cd72f39bcdde984385094f
Natthamon Kharoubi <natthamon.ng@gmail.com>
2020-04-08 12:05:40 +0200

    get pro posts in WallPro using redux action

--- web/src/routing/MainRoutes.js
+++ web/src/routing/MainRoutes.js
@@ -3,7 +3,7 @@ import { Route, Switch } from 'react-router-dom';
 import PrivateRoute, { Admin, Partner, Visitor } from './PrivateRoute';
 
 import Home from '../containers/Home/Home';
-import Wall from '../containers/Wall/Wall';
+import WallPro from '../containers/WallPro/WallPro';
 import AddUser from '../containers/Settings/AddUser';
 import _404 from '../components/_404';
 
@@ -12,7 +12,7 @@ const Routes = () => {
         <section>
             <Switch>
                 <PrivateRoute exact path='/home' component={Visitor(Home)} />
-                <PrivateRoute exact path='/pro' component={Partner(Wall)} />
+                <PrivateRoute exact path='/pro' component={Partner(WallPro)} />
                 {/*<PrivateRoute path="/wall/:filter" component={Visitor(Wall)} />*/}
                 <PrivateRoute exact path='/settings/add-user' component={Admin(AddUser)} />
                 <Route component={_404} />
