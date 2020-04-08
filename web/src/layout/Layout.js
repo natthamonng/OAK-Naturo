dd08b8f1807c6d07d25390c312e5ce92eabd7293
Natthamon Kharoubi <natthamon.ng@gmail.com>
2020-04-08 02:00:00 +0200

    create layout

---
+++ web/src/layout/Layout.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { signOutUser } from '../actions/auth.actions';
+import Navbar from '../components/Navbar';
+import MainRoutes from '../routing/MainRoutes';
+import Footer from '../components/Footer';
+
+const Layout = (props) => {
+    return (
+        <div className="app-admin-wrap layout-horizontal-bar">
+            <Navbar {...props} />
+            <div className="main-content-wrap d-flex flex-column">
+                <MainRoutes />
+            <Footer/>
+            </div>
+        </div>
+    )
+};
+
+Layout.propTypes = {
+    auth: PropTypes.object.isRequired,
+    signOutUser: PropTypes.func.isRequired
+};
+
+const mapStateToProps = state => ({
+    auth: state.auth
+});
+
+export default connect(mapStateToProps,{ signOutUser })(Layout);
