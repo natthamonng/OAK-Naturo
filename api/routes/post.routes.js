62060f02bbbbc8cfdbd4b316822a25c7f500cfbb
Natthamon Kharoubi <natthamon.ng@gmail.com>
2020-04-19 01:20:48 +0200

    Change directory name for uploading file

--- api/routes/post.routes.js
+++ api/routes/post.routes.js
@@ -2,13 +2,14 @@ const express = require("express");
 const router = express.Router();
 const postController = require("../controllers/post.controller");
 const { passportJwt, verifyAuthority } = require("../middlewares");
+require('dotenv').config();
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const upload = multer({dest: process.env.DIR_UPLOAD_POST});
 
 router.get('/:filters', [passportJwt.authenticateJwt], postController.getPostsByFilters);
 
 router.post('/', [passportJwt.authenticateJwt], upload.array('file'),
-        postController.addNewPost, postController.getPostById);
+    postController.addNewPost, postController.getPostById);
 
 router.put('/:postId', [passportJwt.authenticateJwt], postController.unPublishPost);
 
