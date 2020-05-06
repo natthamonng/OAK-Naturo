f66635c5fd46f20d597e0fdf87763f4e41d6970a
Natthamon Kharoubi <natthamon.ng@gmail.com>
2020-05-06 23:44:00 +0200

    Add Helmet package for managing document heads

--- web/src/components/Breadcrumb.js
+++ web/src/components/Breadcrumb.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
 const BreadCrumb = (props) => {
     return (
         <div className="breadcrumb">
+            <Helmet>
+                <meta charSet="utf-8" />
+                <title>Oak - {props.pageName}</title>
+            </Helmet>
             <h1>{props.pageName}</h1>
             <ul>
                 <li><NavLink to={`${props.mainPath}`}>{props.mainName}</NavLink></li>
